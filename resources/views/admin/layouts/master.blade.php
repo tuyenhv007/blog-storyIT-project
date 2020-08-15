a63ecc57a436dbe3148c048a68fbb4bc3208cd27
tuyenhoang0320i1 <hoangtuyen9x@gmail.com>
2020-08-15 22:21:25 +0700

    completed make layouts admin

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <title>@yield('title')</title>
+    <base href="{{asset('')}}">
+    <link href="css/styles.css" rel="stylesheet"/>
+    @toastr_css
+    <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet"
+          crossorigin="anonymous"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"
+            crossorigin="anonymous"></script>
+</head>
+<body class="sb-nav-fixed">
+@include('admin.core.menu')
+<div id="layoutSidenav">
+    @include('admin.core.side-bar')
+    <div id="layoutSidenav_content">
+        <main>
+            <div class="container-fluid">
+                @yield('content')
+            </div>
+        </main>
+
+        <footer class="py-4 bg-light mt-auto">
+            @include('admin.core.footer')
+        </footer>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"
+        crossorigin="anonymous"></script>
+<script src="js/scripts.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+<script src="assets/demo/chart-area-demo.js"></script>
+<script src="assets/demo/chart-bar-demo.js"></script>
+<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+<script src="assets/demo/datatables-demo.js"></script>
+<script src="{{asset('js/ckeditor/ckeditor.js')}}"></script>
+<script src="{{asset('js/ckfinder/ckfinder.js')}}"></script>
+<script>
+    CKEDITOR.replace('editor');
+</script>
+@jquery
+@toastr_js
+@toastr_render
+</body>
+</html>
