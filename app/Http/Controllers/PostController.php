361870f131ab5fe1438dc174502cd7dec45fa183
tuyenhoang0320i1 <hoangtuyen9x@gmail.com>
2020-08-17 21:13:48 +0700

    completed CRUD Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -34,4 +34,40 @@ class PostController extends Controller
         toastr()->success('Bài viết đã được xuất bản!');
         return redirect()->route('post.index');
     }
+
+    public function detailPostUI($id)
+    {
+        $post = $this->postService->findById($id);
+        return view('detail', compact('post'));
+    }
+
+    public function detailPostAdmin($id)
+    {
+        $post = $this->postService->findById($id);
+        return view('admin.post.detail', compact('post'));
+    }
+
+    public function edit(CategoryService $categoryService, $id)
+    {
+        $categories = $categoryService->getAll();
+        $post = $this->postService->findById($id);
+        return view('admin.post.edit', compact('post','categories'));
+    }
+
+    public function update(PostRequest $postRequest, $id)
+    {
+        $post = $this->postService->findById($id);
+        $this->postService->update($postRequest, $post);
+        toastr()->success('Cập nhập bài viết thành công!');
+        return redirect()->route('post.index');
+
+    }
+
+    public function delete($id)
+    {
+        $post = $this->postService->findById($id);
+        $this->postService->delete($post);
+        toastr()->success('Xóa bài viết thành công!');
+        return redirect()->route('post.index');
+    }
 }
